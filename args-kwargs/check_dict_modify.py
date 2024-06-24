a0498d21e299e3f5e2312aee57ef03d7e07632c2
ASergeevich <cs.muravey@gmail.com>
2024-06-24 15:24:47 -0400

    added one more type of results output

--- args-kwargs/check_dict_modify.py
+++ args-kwargs/check_dict_modify.py
@@ -1,4 +1,4 @@
-def is_modify_dict(old_dict: dict, **kwargs,) -> tuple[dict, bool]:
+def is_modify_dict(old_dict: dict, **kwargs) -> tuple[dict, bool]:
     is_modified = False
     modified_fields = []
     for key, value in kwargs.items():
@@ -12,4 +12,9 @@ def is_modify_dict(old_dict: dict, **kwargs,) -> tuple[dict, bool]:
 product_1 = {'id': 1, 'name': 'Laptop', 'price': 1000}
 product_2 = {'id': 1, 'name': 'New laptop', 'price': 800}
 
-print(is_modify_dict(old_dict=product_1, **product_2))
+# print(is_modify_dict(old_dict=product_1, **product_2)) #result in 1 line
+
+original_item, is_modified, modified_fields = is_modify_dict(product_1, **product_2) #result in some lines
+print(original_item)
+print(is_modified)
+print(modified_fields)
