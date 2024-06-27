1d5eb1ef17a54d39f71a0f1a9f24e8943b8c7719
ASergeevich <cs.muravey@gmail.com>
2024-06-27 09:57:37 -0400

    final stage of binance rate api

--- my_api_exp/my_api_exp.py
+++ my_api_exp/my_api_exp.py
@@ -1,12 +1,13 @@
 import requests
+import time
 
 url = 'https://api.binance.com/api/v3/ticker/price'
 
-response = requests.get(url, params={'symbol': 'BTCUSDT'})
+list_of_rates = []
 
-price_object = response.json()
-price = float(price_object['price'])
-
-
-print(price)
-print(type(price))
+for i in range(5):
+    response_eth = requests.get(url, params={'symbol': 'ETHUSDT'})
+    price_eth = float(response_eth.json()['price'])
+    list_of_rates.append(price_eth)
+    time.sleep(1)
+print(list_of_rates)
