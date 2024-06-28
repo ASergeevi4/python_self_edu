f1170bd8ed562971991cbb2d04874a21cec4cc87
ASergeevich <cs.muravey@gmail.com>
2024-06-27 20:59:00 -0400

    some changes

---
+++ compreh_sets/comp_and_sets.py
@@ -0,0 +1,13 @@
+# first list option
+
+# squares = [i ** 2 for i in range(1,10)]
+# even_squares = [x ** 2 for x in range(10) if x % 2 == 0]
+
+# print(even_squares)
+
+#second list option
+# numbers = [1, 2, 3, 4, 5, 6, 6, 7, 7, 8]
+# labeled_numbers = ['even' if x % 2 == 0 else 'odd' for x in numbers]
+
+# print(labeled_numbers)
+
