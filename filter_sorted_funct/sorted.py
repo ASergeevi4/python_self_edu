910ecc218beb9e21a25e0f65cfd26596af313910
ASergeevich <cs.muravey@gmail.com>
2024-06-28 16:06:30 -0400

    intermediate commit with a ready list sorter

--- filter_sorted_funct/sorted.py
+++ filter_sorted_funct/sorted.py
@@ -1,1 +1,13 @@
+#       ========== sort with list
+fruit = ['banana', 'apple', 'cherry', 'date'] 
+sorted_fruit = sorted(fruit, reverse=True)  # linear sort
+# print(sorted_fruit)
 
+def sort_by_len(element: str) -> int:   # funct to indicate lenght element of (list for example)
+    return len(element)
+
+sorted_fruit = sorted(fruit, key=sort_by_len)
+# print(sorted_fruit)
+
+
+#       ============
