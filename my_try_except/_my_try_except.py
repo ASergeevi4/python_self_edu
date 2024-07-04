06dcc50f9781192dd18a4e92c82684218d42c802
ASergeevich <cs.muravey@gmail.com>
2024-07-04 17:53:13 -0400

    final version

--- my_try_except/_my_try_except.py
+++ my_try_except/_my_try_except.py
@@ -9,16 +9,18 @@ import requests, time
 #     print("The list is empty")
 
 try:
-
-    good_value = []
-    for i in range(10):
+    amount_of_requests = int(input("Enter amount of requests (only integers): "))
+    min_rate = float(input("Enter minimum rate in range: "))
+    max_rate = float(input("Enter maximum rate in range: "))
+    good_value = {}
+    for i in range(amount_of_requests):
         response = requests.get('https://api.binance.com/api/v3/ticker/price', params={'symbol': 'BTCUSDT'})
         bitcoin_price = float(response.json()["price"])
         time.sleep(1)
         print(bitcoin_price)
-        if bitcoin_price >= 58400:
-            good_value.append(bitcoin_price)
-    print(good_value)
+        if bitcoin_price >= min_rate and bitcoin_price <= max_rate:
+            good_value[time.ctime()] = bitcoin_price
+    print("======", f"Amount in collection is {len(good_value)}", good_value, sep="\n")
 
 except requests.exceptions.ConnectionError as error: # if there no internet or signal lost
     print(f"The problem with {error}")
