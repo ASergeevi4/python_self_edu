fb741151ff699e2a62a8b2402a3330b99fef30b8
ASergeevich <cs.muravey@gmail.com>
2024-07-04 17:23:58 -0400

    working version before final update

--- my_try_except/_my_try_except.py
+++ my_try_except/_my_try_except.py
@@ -1,4 +1,4 @@
-import requests, time, datetime
+import requests, time
 
 # def find_average(*, numbers: list) -> float:
 #     return sum(numbers) / len(numbers)
@@ -10,15 +10,15 @@ import requests, time, datetime
 
 try:
 
-    good_value = {}
+    good_value = []
     for i in range(10):
         response = requests.get('https://api.binance.com/api/v3/ticker/price', params={'symbol': 'BTCUSDT'})
         bitcoin_price = float(response.json()["price"])
         time.sleep(1)
         print(bitcoin_price)
-        if bitcoin_price >= 62092.01:
+        if bitcoin_price >= 58400:
             good_value.append(bitcoin_price)
-            time_of_best.append(datetime.datetime.now())
-    print(len(good_value), good_value, time_of_best)
+    print(good_value)
+
 except requests.exceptions.ConnectionError as error: # if there no internet or signal lost
     print(f"The problem with {error}")
